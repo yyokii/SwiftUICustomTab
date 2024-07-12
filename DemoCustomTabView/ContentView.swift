ab3bd2ea5bdc12e3506b1f03263c8debcbc97c32
yyokii <yyokii.h@gmail.com>
2024-07-12 22:30:22 +0900

    Initial Commit

---
+++ DemoCustomTabView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoCustomTabView
+//
+//  Created by Higashihara Yoki on 2024/07/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoCustomTabView/DemoCustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCustomTabViewApp.swift
+//  DemoCustomTabView
+//
+//  Created by Higashihara Yoki on 2024/07/12.
+//
+
+import SwiftUI
+
+@main
+struct DemoCustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
